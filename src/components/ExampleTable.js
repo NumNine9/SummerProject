ffcbbf0e63bd64c032c8345f573e843085fc950a
George Nyagura <starboy@Georges-MacBook-Air.local>
2021-05-06 14:23:53 +0200

    no change im just testing this git command

---
+++ src/components/ExampleTable.js
@@ -0,0 +1,83 @@
+
+import React from 'react'
+
+function ExampleTable() {
+  return (
+    <div>
+      
+<div >
+  <div >
+    <div >
+      <div >
+        <table >
+          <thead >
+            <tr>
+              <th >
+                Name
+              </th>
+              <th >
+                Title
+              </th>
+              <th >
+                Email
+              </th>
+              <th >
+                Role
+              </th>
+              <th >
+                <span >Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+          
+            <tr >
+              <td >
+                Jane Cooper
+              </td>
+              <td >
+                Regional Paradigm Technician
+              </td>
+              <td >
+                jane.cooper@example.com
+              </td>
+              <td >
+                Admin
+              </td>
+              <td >
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+              </td>
+            </tr>
+
+            
+            <tr >
+              <td >
+                Cody Fisher
+              </td>
+              <td >
+                Product Directives Officer
+              </td>
+              <td >
+                cody.fisher@example.com
+              </td>
+              <td >
+                Owner
+              </td>
+              <td>
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+              </td>
+            </tr>
+
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </div>
+  )
+}
+
+export default ExampleTable
